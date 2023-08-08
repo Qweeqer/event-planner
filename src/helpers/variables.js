79e2917e514e69eaac72b471d754f79c993b24f5
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-08 16:27:57 +0300

    Create variables.js

---
+++ src/helpers/variables.js
@@ -0,0 +1,50 @@
+export const categories = [
+  { valueName: "Art", value: "Art" },
+  { valueName: "Music", value: "Music" },
+  { valueName: "Business", value: "Business" },
+  { valueName: "Conference", value: "Conference" },
+  { valueName: "Workshop", value: "Workshop" },
+  { valueName: "Party", value: "Party" },
+  { valueName: "Sport", value: "Sport" },
+];
+
+export const priorities = [
+  { valueName: "Low", value: 1 },
+  { valueName: "Medium", value: 2 },
+  { valueName: "High", value: 3 },
+];
+
+export const sortValues = [
+  {
+    name: "name",
+    property: "title",
+    order: "asc",
+  },
+  {
+    name: "name",
+    property: "title",
+    order: "desc",
+  },
+
+  {
+    name: "date",
+    property: "date",
+    order: "asc",
+  },
+  {
+    name: "date",
+    property: "date",
+    order: "desc",
+  },
+
+  {
+    name: "priority",
+    property: "priority",
+    order: "asc",
+  },
+  {
+    name: "priority",
+    property: "priority",
+    order: "desc",
+  },
+];
