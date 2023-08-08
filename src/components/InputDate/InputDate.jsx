bf84e468dce9746e7ef92678d4cc0a3626ac9066
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-08 16:27:36 +0300

    Create InputDate.jsx

---
+++ src/components/InputDate/InputDate.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useRef } from "react";
+import Calendar from "react-calendar";
+import { format, parseISO } from "date-fns";
+import useOutsideClick from "../../hooks/useClickOutside";
+import {
+  Wrap,
+  WrapInput,
+  Input,
+  StyledIconUp,
+  StyledIconDown,
+  Popup,
+  Text,
+  BtnWrap,
+  BtnCancel,
+  BtnChoose,
+} from "./InputDate.styled";
+import "react-calendar/dist/Calendar.css";
+
+function InputDate({ field, form, options, label, meta, ...props }) {
+  const formValue = field.value ? parseISO(field.value) : null;
+  const [value, setValue] = useState(formValue);
+  const [changeValue, setChangeValue] = useState(formValue);
+  const [isOpen, setIsOpen] = useState(false);
+  const inputRef = useRef(null);
+
+  const outsideClickHandler = () => {
+    setIsOpen(false);
+  };
+
+  useOutsideClick(inputRef, outsideClickHandler);
+
+  const togglePopup = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const onChange = (value) => {
+    setChangeValue(value);
+  };
+
+  const onChoose = (value) => {
+    setValue(value);
+    togglePopup();
+
+    form.setFieldValue(field.name, value);
+  };
+
+  return (
+    <Wrap ref={inputRef}>
+      <WrapInput>
+        <Input onClick={togglePopup}>
+          <Text $select={value}>
+            {!isOpen && <>{value ? format(value, "dd.MM") : "Select"}</>}
+            {isOpen && `Select ${label}`}
+          </Text>
+        </Input>
+        {!isOpen ? (
+          <StyledIconDown onClick={togglePopup} color="#7B61FF" />
+        ) : (
+          <StyledIconUp onClick={togglePopup} color="#7B61FF" />
+        )}
+      </WrapInput>
+
+      {isOpen && (
+        <Popup>
+          <Calendar
+            calendarType="gregory"
+            locale="en-EN"
+            next2Label={null}
+            prev2Label={null}
+            showNeighboringMonth={false}
+            onChange={onChange}
+            value={changeValue}
+          />
+          <BtnWrap>
+            <BtnCancel onClick={togglePopup} type="button">
+              Cancel
+            </BtnCancel>
+            <BtnChoose
+              onClick={() => {
+                onChoose(changeValue);
+              }}
+              type="button"
+            >
+              Choose date
+            </BtnChoose>
+          </BtnWrap>
+        </Popup>
+      )}
+    </Wrap>
+  );
+}
+
+export default InputDate;
