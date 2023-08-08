856d706d567d9ecb5630697d825acc216ea4892a
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-08 20:07:12 +0300

    add animation

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,14 +1,24 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { device } from "../../utils/device";
 import bg from "../../assets/images/backgroundImage.svg";
 
+const waveAnimation = keyframes`
+  0% {
+    background-position: center;
+  }
+  100% {
+    background-position: center -50px;
+  }
+`;
+
 export const StyledMain = styled.main`
   background-image: url(${bg});
-
   background-position: center;
   background-size: cover;
-  min-height: 100vh;
+  background-repeat: no-repeat;
+  min-height: 100%;
   padding: 40px 0 100px;
+  animation: ${waveAnimation} 3s linear 6 alternate;
 
   @media ${device.desktop} {
     padding: 60px 0;
