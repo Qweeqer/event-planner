2c261dceb1cafd6cfef54237521ab7a88960674e
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-08 16:21:26 +0300

    Create Language.jsx

---
+++ src/components/Language/Language.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Wrap } from "./Language.styled";
+
+function Language() {
+  return (
+    <Wrap type="button">
+      <option value="en">En</option>
+      <option value="uk">UK</option>
+    </Wrap>
+      
+  );
+}
+
+export default Language;
