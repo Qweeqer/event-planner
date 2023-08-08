20181ed5676146d0c6682194356506fef591a0ac
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-08-08 16:23:08 +0300

    Create CreateForm.jsx

---
+++ src/components/CreateForm/CreateForm.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import EventForm from "../EventForm/EventForm";
+import { useDispatch } from "react-redux";
+import { addEvent } from "../../redux/events/eventsOperations";
+
+function CreateForm() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const initialValues = {
+    title: "",
+    description: "",
+    date: null,
+    time: "",
+    location: "",
+    category: "",
+    picture: "",
+    priority: null,
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(addEvent(values));
+    resetForm();
+    navigate("/");
+  };
+
+  return (
+    <EventForm
+      onSubmit={handleSubmit}
+      initialValues={initialValues}
+      btnText="Add event"
+    />
+  );
+}
+
+export default CreateForm;
